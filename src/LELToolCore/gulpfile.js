b53e4705d19161824cd177b56d421749b3a26b98
msarmi <bigot-x@hotmail.com>
2016-10-17 08:05:47 -0300

    Hello World Angular

---
+++ src/LELToolCore/gulpfile.js
@@ -0,0 +1,36 @@
+﻿/// &lt;binding BeforeBuild='inject:index' />
+//"use strict";
+
+//var gulp = require("gulp"),
+//    series = require('stream-series'),
+//    inject = require('gulp-inject'),
+//    wiredep = require('wiredep').stream;
+
+////var webroot = "./wwwroot/";
+//var webroot = "./";
+
+//var paths = {
+//	ngModule: webroot + "Scripts/**/*.module.js",
+//	ngRoute: webroot + "Scripts/**/*.route.js",
+//	ngController: webroot + "Scripts/**/*.controller.js",
+//	script: webroot + "Scripts/**/*.js",
+//	style: webroot + "/wwwroot/css/**/*.css"
+//};
+
+//gulp.task('inject:index', function () {
+//	var moduleSrc = gulp.src(paths.ngModule, { read: false });
+//	var routeSrc = gulp.src(paths.ngRoute, { read: false });
+//	var controllerSrc = gulp.src(paths.ngController, { read: false });
+//	var scriptSrc = gulp.src(paths.script, { read: false });
+//	var styleSrc = gulp.src(paths.style, { read: false });
+
+//	gulp.src(webroot + 'Views/Home/index.cshtml')
+//        .pipe(wiredep({
+//        	optional: 'configuration',
+//        	goes: 'here',
+//        	ignorePath: '..'
+//        }))
+//        .pipe(inject(series(scriptSrc, moduleSrc, routeSrc, controllerSrc), { ignorePath: '/wwwroot' }))
+//        .pipe(inject(series(styleSrc), { ignorePath: '/wwwroot' }))
+//        .pipe(gulp.dest(webroot + 'app'));
+//});
---
+++ src/LELToolCore/wwwroot/js/app.js
@@ -0,0 +1,7 @@
+﻿(function () {
+	var app = angular.module('app', []);
+	// A controller that displays hello world
+	app.controller('HomeCtrl', function () {
+		this.message = "Hello, world";
+	});
+})();
